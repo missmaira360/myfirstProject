6961b1c75e094f9a5bb511cd1ab6ae4f4cf48445
missmaira360 <105669662+missmaira360@users.noreply.github.com>
2023-09-01 17:00:03 +0300

    home page commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,79 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> hi emiliana welcome to laravel</h1>
-    <h2> new line</h2>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" >
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+
+  </head>
+  <body style="background-color:grey">
+       <div>
+        <nav class="navbar navbar-expand-sm bg-light " style="box-shadow: 0 .125rem .25rem rgb(146, 143, 143);;">
+            <div class="container-fluid justify-content-end">
+              
+                <ul class="navbar-nav">
+                    <li class="nav-item pl-4">
+                        <a class="nav-link" href="home">home</a>
+                    </li>
+
+                    <li class="nav-item pl-4">
+                        <a class="nav-link" href="contact">contact</a>
+                    </li>
+                    
+                    <li class="nav-item pl-4">
+                        <a class="nav-link" href="AboutUs">About Us</a>
+                    </li>
+                    <li class="nav-item pl-4">
+                        <a class="nav-link" href="login">login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+<br>
+ <div>
+   <h1 style="font-family: fantasy; text-align: center;">Welcome to Pangisha Online</h1>
+ </div>
+
+  <div class="row mt-5" style="height: 15rem;  ">
+            <div class="col-lg-4">
+                <img class="animated" style="height: 15rem; width: 100%;" src="{{ URL ('images/house1.jpg')}}">
+            </div>
+            <div class="col-lg-8 justify-content-center " style="margin: auto;">
+                <h1>Best house</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolores labore porro architecto libero, perferendis quidem itaque numquam minus earum ducimus iusto vitae mollitia tempore vel quia consequatur rerum . Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quaerat amet vel eveniet reiciendis, repellendus ducimus qui rem quasi ex obcaecati facere nobis quis in omnis praesentium consequatur molestiae atque?</p>
+                <div>
+                    <!-- <a href="Gallery2.html"><button class="btn btn-success" style="display: block; float: right; margin-bottom: 15px">Click here</button></a> -->
+                </div>
+            </div>
+            
+            
+        </div>
+
+        <div class="row mt-5" style="height: 15rem; ">
+            <div class="col-lg-8 justify-content-center " style="margin: auto;">
+                <h1>affordable price.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolores labore porro architecto libero, perferendis quidem itaque numquam minus earum ducimus iusto vitae mollitia tempore vel quia consequatur rerum . Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quaerat amet vel eveniet reiciendis, repellendus ducimus qui rem quasi ex obcaecati facere nobis quis in omnis praesentium consequatur molestiae atque?</p>
+                <div>
+                    <!-- <a href="Gallery3.html"><button class="btn btn-success" style="display: block; float: left; margin-bottom: 15px">Click here</button></a> -->
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <img class="animated" style="height: 15rem; width: 100%;" src="{{ URL ('images/house2.jpg')}}">
+            </div>
+            
+        </div>
+
+
+ 
+
 </body>
 </html>
+
+
+
+
+    
